c3d3da646c421014875687aaba2518a3bb46db25
arrowrowe <arrowrowe@gmail.com>
2016-04-22 07:23:04 +0800

    :sparkles: A More Semantic Solution for C - Demo Day
    And more accurate!

--- c-demo-day/app.py
+++ c-demo-day/app.py
@@ -4,8 +4,8 @@ ensureValue = lambda fn, *arg: fn(*arg) if hasattr(fn, '__call__') else fn
 def getPack2d(N, M, fillers):
   pack = [[0] * M for _ in xrange(N)]
   pack[0][0] = ensureValue(fillers[0], pack)
-  for i in xrange(1, M):
-    pack[0][i] = ensureValue(fillers[1], pack, i)
+  for j in xrange(1, M):
+    pack[0][j] = ensureValue(fillers[1], pack, j)
   for i in xrange(1, N):
     pack[i][0] = ensureValue(fillers[2], pack, i)
   fn = fillers[3]
@@ -15,25 +15,37 @@ def getPack2d(N, M, fillers):
   return pack[N - 1][M - 1]
 
 def getMinToChange(N, M, maze):
-  total = N * M
+  COME_FROM_TOP = 0
+  COME_FROM_LEFT = 1
+  infinity = N * M
+  emptyVi = lambda i, j: (1 - maze[i][j]) if i < N else 0
+  emptyVj = lambda i, j: (1 - maze[i][j]) if j < M else 0
+  initializer = [1, 1] if maze[0][0] else [0, 0]
+  columnInitializer = emptyVj(0, 1)
   return min(getPack2d(N, M, [
-    [0, 0],
-    lambda pack, i: [total, pack[0][i - 1][1] + int(maze[0][i])],
-    [total, total],
+    initializer,
+    lambda pack, j: [
+      infinity,
+      pack[0][j - 1][COME_FROM_LEFT] + maze[0][j]
+    ],
+    lambda pack, i: [columnInitializer, infinity] if i == 1 else [
+      pack[i - 1][0][COME_FROM_TOP] + maze[i][0],
+      infinity
+    ],
     lambda pack, i, j: [
       min(
-        pack[i - 1][j][0],
-        pack[i - 1][j][1] + int(j + 1 < M and not maze[i - 1][j + 1])
-      ) + int(maze[i][j]),
+        pack[i - 1][j][COME_FROM_TOP],
+        pack[i - 1][j][COME_FROM_LEFT] + emptyVj(i - 1, j + 1)
+      ) + maze[i][j],
       min(
-        pack[i][j - 1][1],
-        pack[i][j - 1][0] + int(i + 1 < N and not maze[i + 1][j - 1])
-      ) + int(maze[i][j])
+        pack[i][j - 1][COME_FROM_LEFT],
+        pack[i][j - 1][COME_FROM_TOP] + emptyVi(i + 1, j - 1)
+      ) + maze[i][j]
     ]
   ]))
 
 def main():
   N, M = readSplitted()
-  print getMinToChange(N, M, [[x == 'b' for x in raw_input()] for _ in xrange(N)])
+  print getMinToChange(N, M, [[int(x == 'b') for x in raw_input()] for _ in xrange(N)])
 
 main()
